cb254a908c6695eb62dd01fc1b13e7d38db4e21b
muthukaruppan PL <muthu2006k@gmail.com>
2025-12-12 13:26:26 +0530

    updated app.py and index.html

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-import pickle
 import numpy as np
+import pickle
 
 app = Flask(__name__)
 
-# Load your model
 model = pickle.load(open("car_price_model.pkl", "rb"))
 
 @app.route("/")
@@ -14,14 +13,16 @@ def home():
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
+        name = int(request.form.get("name"))
         year = int(request.form.get("year"))
-        km_driven = int(request.form.get("km"))
+        km = int(request.form.get("km"))
         fuel = int(request.form.get("fuel"))
         seller = int(request.form.get("seller"))
         transmission = int(request.form.get("transmission"))
         owner = int(request.form.get("owner"))
 
-        features = np.array([[year, km_driven, fuel, seller, transmission, owner]])
+        features = np.array([[name, year, km, fuel, seller, transmission, owner]])
+
         prediction = model.predict(features)[0]
 
         return render_template("index.html", result=round(prediction, 2))
