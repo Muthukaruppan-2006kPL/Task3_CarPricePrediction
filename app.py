ccd8130234cef4cb336934d00f4c96d5096e9412
muthukaruppan PL <muthu2006k@gmail.com>
2025-12-17 12:43:30 +0530

    updated app.py and index.html

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, request
 import pickle
-import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
+# Load model
 model = pickle.load(open("car_price_model.pkl", "rb"))
 
 @app.route("/")
@@ -15,36 +16,31 @@ def predict():
     try:
         year = int(request.form["year"])
         km_driven = int(request.form["km_driven"])
-
         fuel = request.form["fuel"]
         seller = request.form["seller"]
         transmission = request.form["transmission"]
         owner = request.form["owner"]
 
-        fuel_petrol = 1 if fuel == "Petrol" else 0
-        fuel_diesel = 1 if fuel == "Diesel" else 0
+        input_df = pd.DataFrame([{
+            "name": "Maruti",
+            "year": year,
+            "km_driven": km_driven,
+            "fuel": fuel,
+            "seller_type": seller,
+            "transmission": transmission,
+            "owner": owner
+        }])
 
-        seller_individual = 1 if seller == "Individual" else 0
-
-        transmission_manual = 1 if transmission == "Manual" else 0
-
-        owner_first = 1 if owner == "First Owner" else 0
-        owner_second = 1 if owner == "Second Owner" else 0
-
-        input_data = np.array([[year, km_driven,
-                                fuel_diesel, fuel_petrol,
-                                seller_individual,
-                                transmission_manual,
-                                owner_first, owner_second]])
-
-        prediction = model.predict(input_data)[0]
+        prediction = model.predict(input_df)[0]
+        prediction = round(prediction, 2)
 
         return render_template(
             "index.html",
-            prediction_text=f"₹ {round(prediction, 2)}"
+            prediction_text=f"₹ {prediction}"
         )
 
     except Exception as e:
+        print(e)
         return render_template(
             "index.html",
             prediction_text="Error occurred"
